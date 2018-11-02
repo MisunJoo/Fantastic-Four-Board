7f949a98037ce8378a7dbc026c82ef51767411a5
Shin, Yun-Cheol <younchul.shin@gmail.com>
2018-11-02 20:25:13 +0900

    feat : file service 작성중

--- src/main/java/project/ffboard/service/ArticleServiceImpl.java
+++ src/main/java/project/ffboard/service/ArticleServiceImpl.java
@@ -2,11 +2,20 @@ package project.ffboard.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import project.ffboard.dao.ArticleDao;
 import project.ffboard.dto.Article;
 import project.ffboard.dto.ArticleContent;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class ArticleServiceImpl implements ArticleService {
     private int limit = 10; //한페이지에 보여주는 최대 게시글의 갯수
@@ -80,5 +89,46 @@ public class ArticleServiceImpl implements ArticleService {
     @Transactional(readOnly = true)
     public List<Article> getArticleList(int categoryId, int start, String searchType, String searchWord) {
         return articleDao.getArticleList(categoryId,start,limit,searchType,searchWord);
+    }
+
+    public int upload(MultipartFile file, Long articleId) {
+
+        //file upload
+        UUID uuid = UUID.randomUUID();
+        String uuidStr = uuid.toString();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String dataStr = simpleDateFormat.format(new Date());
+
+        String baseDir = "/home/jycs/tmp";
+        String saveDir = baseDir + "/" + dataStr;
+        String saveFile = saveDir + "/" + uuidStr;
+
+
+        File fileObj = new File(saveDir);
+        fileObj.mkdirs();
+
+        InputStream in = null;
+        OutputStream out = null;
+
+        try{
+            in = file.getInputStream();
+            out = new FileOutputStream(saveFile);
+            byte[] buffer = new byte[1024];
+            int readCount = 0;
+            while((readCount = in.read(buffer)) != -1) {
+                out.write(buffer, 0, readCount);
+            }
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }finally {
+            if(in != null) {
+                try {in.close();} catch (Exception e) {}
+            }
+            if(out != null) {
+                try {out.close();} catch (Exception e) {}
+            }//file upload
+        }
+        return ;
     }
 }
